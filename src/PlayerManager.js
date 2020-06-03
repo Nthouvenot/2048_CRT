3cac92cd819031252941a57a853de96bfe0a6571
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-03 16:56:36 +0200

    Create PlayerManager.js
    démarrage de l'implémentation de la classePlayerManager

---
+++ src/PlayerManager.js
@@ -0,0 +1,31 @@
+
+class PlayerManager {
+
+    constructor() {
+        this.player = new Array;
+    }
+
+    create(_player) {
+        if (_player instanceof Player) {
+
+        } else {
+            return false;
+        }
+    }
+
+    read() { //todo parameter not defined
+
+    }
+
+    update(_player) {
+        if (_player instanceof Player) {
+
+        } else {
+            return false;
+        }
+    }
+
+    delete() { //todo parameter not defined
+
+    }
+}
