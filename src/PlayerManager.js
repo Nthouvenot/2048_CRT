37385bb29d18c8364b434420e68e1f1be1c3b33d
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-03 18:25:08 +0200

    Update PlayerManager.js
    Implementation de la classe PlayerManager

--- src/PlayerManager.js
+++ src/PlayerManager.js
@@ -1,31 +1,63 @@
+const player = require('./Player.js')
+
 
 class PlayerManager {
 
     constructor() {
-        this.player = new Array;
+        this.players = [];
     }
 
+    /**
+     * Ajoute un joueur a la clollection d'objet de type Player
+     * @param {Player} _player
+     * @return {boolean}
+     */
     create(_player) {
-        if (_player instanceof Player) {
-
-        } else {
+        if (!(_player instanceof Player)) {
             return false;
         }
+        this.players.push(_player);
     }
 
-    read() { //todo parameter not defined
-
+    /**
+     * Lit un player de la collection d'objet de type Player d'aprés son username
+     * @param {string} _username
+     * @returns {Player}
+     */
+    read(_username) {
+        if (!(typeof _username == 'string')) {
+            return undefined;
+        }
+        let playerReader = this.players.find(player => player.username == _username);
+        if (playerReader != undefined) {
+            return undefined;
+        }
+        return Object.assign(playerReader);
     }
 
     update(_player) {
-        if (_player instanceof Player) {
+        if (!(_player instanceof 'Player')) {
+            return undefined;
+        }
+        if (!this.delete(_player.username)) {
+            return undefined;
+        }
+        this.create(_player);
+    }
 
-        } else {
+    /**
+     * Supprime un joueur en fonction de son username
+     * @param {string} _username
+     * @returns {boolean}
+     */
+    delete(_username) {
+        if (!(typeof _username == 'string')) {
             return false;
         }
-    }
-
-    delete() { //todo parameter not defined
-
-    }
+        let index = this.players.indexOf('username', _username);
+        if (index == undefined) {
+            return false;
+        }
+        this.players.splice(index, 1);
+        return true;
 }
