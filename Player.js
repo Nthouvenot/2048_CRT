f362ef9cabba84eb75bbb1ba5494f3fa2cad09cd
Masoud RAVANDOUST <akam_50@yahoo.com>
2020-06-03 15:05:50 +0200

    La classe Player

--- Player.js
+++ Player.js
@@ -1,3 +1,15 @@
 class Player {
+    constructor(_username, _password, _email, _totalGames, _totalScore, _dateCreation, _lastLogged, _isLogged) {
+        this.username = '_username' || 'any';
+        this.password = '_password' || 'one';
+        this.email = '_email' || 'undefinde';
+        this.totalGames = parseInt(_totalGames);
+        this.totalScore = parseInt(_totalScore);
+        this.dateCreation = _dateCreation;
+        this.lastLogged = _lastLogged;
+        this.isLogged = _isLogged || false;
+    }
+
+
 
 }
