7284fb174444b622a7f80bca13475b1b910aabf2
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-18 17:18:15 +0200

    Update PlayerManager.js
    Amélioration des regex

--- tests/PlayerTest.js
+++ tests/PlayerTest.js
@@ -8,8 +8,24 @@ const Player = require('../src/Player.js');
 
 let player1 = new Player("Xgamer", "1j5m854i2n", "xgamer@gmail.com", 0, 0, Date.now(), Date.now(), Date.now(), true);
 
+console.log("Affichage des attributs par les getters : ")
 console.log("nom du joueur : " + player1.getUsername());
 console.log("mail du joueur : " + player1.getEmail());
 console.log("date de création du joueur : " + player1.getDateCreation());
+console.log("score du joueur : " + player1.getTotalScore());
+console.log("nombre de partie du joueur : " + player1.getTotalGames());
+
+//On modifie les donnees du joueur
+player1.setUsername("GameMaster");
+player1.setEmail("gamemaster@gmail.com");
+player1.setTotalGames(12);
+player1.setTotalScore(1200);
+player1.setLastLogged(Date.now());
+
+//On reaffiche les donnes apres les modifications
+console.log("Affichage des attributs par les getters apres modification du joueur : ")
+console.log("nom du joueur : " + player1.getUsername());
+console.log("mail du joueur : " + player1.getEmail());
+console.log("date de connexion du joueur : " + player1.getLastLogged());
 console.log("score du joueur : " + player1.getTotalScore());
 console.log("nombre de partie du joueur : " + player1.getTotalGames());
