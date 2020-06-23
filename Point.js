87de1a46e36fd1b21520ed925577fcc48dbcb753
LaetitiaCR <58287147+LaetitiaCR@users.noreply.github.com>
2020-06-23 11:06:39 +0200

    add 3 files
    class Point, ZoneQuadrillee and Number

---
+++ Point.js
@@ -0,0 +1,17 @@
+﻿class Point {
+    /**
+     * Constructeur: Initialise une nouvelle instance de la classe "Point"
+     * @param int _x Coordonnée horizontale du point (abscisse). Valeur négative acceptée
+     * @param int _y Coordonnée verticale du point (ordonnée). Valeur négative acceptée
+     */
+
+    constructor(_x, _y) {
+        this.x = parseInt(_x);
+        this.y = parseInt(_y);
+    }
+
+    toString() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+}
